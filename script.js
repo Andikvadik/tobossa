94b9854b130e56d05f14ebc613b48490dfecaa87
Andikvadik <andriankaxuliganka@gmail.com>
2025-08-01 16:39:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ function showCake() {
     origin: { y: 0.6 }
   });
 }
+function showCake() {
+  const cake = document.getElementById('cake');
+  cake.classList.remove('hidden');
+
+  const greeting = document.getElementById('greeting');
+  greeting.classList.remove('hidden');
+
+  confetti({
+    particleCount: 150,
+    spread: 100,
+    origin: { y: 0.6 }
+  });
+}
+
 
